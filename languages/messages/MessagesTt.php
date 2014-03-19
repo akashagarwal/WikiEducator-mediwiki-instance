0d5bdbaadc58fd370e27c875be3f0d833d67b8c2
Akash Agarwal <akashslg@gmail.com>
2014-03-19 13:13:58 +0530

    Mediwiki + Education Prog Extension

---
+++ languages/messages/MessagesTt.php
@@ -0,0 +1,18 @@
+<?php
+/** Tatar (татарча/tatarça)
+ *
+ * See MessagesQqq.php for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ */
+
+$fallback = 'tt-cyrl, ru';
+
+$messages = array(
+# Delete
+'excontentauthor' => '',
+
+);
